e290844b132d64008cd51e69b092de6a93ae875a
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-10-28 22:52:43 -0700

    installed redux-react, import Provider in app.js wrapped div with Provider and created a empty function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'; //provider react component the glue for react & redux
 
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
+import { createStore,applyMiddleware } from 'redux';
 
+const store = createStore(() => [],{},applyMiddleware())
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Provider store={ store }> //store holds the state
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <PostForm />
-        <hr />
-        <Posts />
-      </header>
-    </div>
+          <PostForm />
+          <hr />
+          <Posts />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
