8ddc36e6bcc6c15f8d913f53a75d2076aa5ff7a4
JOHNSON JOSEPH <johnsonjoseph@JOHNSONs-MacBook-Air.local>
2019-10-28 22:32:53 -0700

    Completed first section with react

---
+++ src/components/Posts.js
@@ -0,0 +1,36 @@
+import React,{ Component } from 'react';
+
+class Posts extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      post: [],
+      posts: []
+    }
+  }
+
+  componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => this.setState({ posts: data }));
+  }
+
+  render() {
+
+    const postItems = this.state.posts.map(post => (
+      <div key={ post.id }>
+        <h3> { post.title }</h3>
+        <p> { post.body }</p>
+      </div>
+    ));
+    return (
+      <div>
+        <h1>Post Johnson</h1>
+        { postItems }
+      </div>
+    );
+  }
+}
+
+export default Posts;
